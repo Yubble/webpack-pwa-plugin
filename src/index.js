48b5a28a5314fee7b1dcea6a01849857764f0fdc
Yubble <hailen91@qq.com>
2022-08-24 15:45:49 +0800

    feat: 更新代码

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import m1 from './module1'
 
 if ('serviceWorker' in navigator) { // 兼容性判断
   window.addEventListener('load', () => {// 等所有资源加载完，再注册serviceWorker
-    navigator.serviceWorker.register('./dist/service-worker.js')// service-worker.js 这个文件由 workbox-webpack-plugin 插件生成
+    navigator.serviceWorker.register('../dist/service-worker.js')// service-worker.js 这个文件由 workbox-webpack-plugin 插件生成
     .then(() => {
       console.log("serviceworker 注册成功");
     })
